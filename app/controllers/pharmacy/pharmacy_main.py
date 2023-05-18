977b989109d160a3b6aa459c8fadeacd8dcfe3b4
dem0-0 <emirkan.derkoken1@gmail.com>
2023-05-18 17:08:57 +0300

    pharmacy

--- app/controllers/pharmacy/pharmacy_main.py
+++ app/controllers/pharmacy/pharmacy_main.py
@@ -1,4 +1,5 @@
 from src.services.base_service import BaseService
+from src.services.database_service import DatabaseService
 
 from flask.views import MethodView
 from flask import render_template
@@ -6,15 +7,19 @@ from flask import render_template
 class PharmacyMain(MethodView, BaseService):
     init_every_request = True   # Must be set to True for authorization, default is also True
 
-    def __init__(self, database_service):
+    def __init__(self, database_service: DatabaseService):
         super().__init__()
         self.database_service = database_service
-    
-    @BaseService.login_required
+        ###############################
+        self.uid = 2
+        self.name = "bilkent eczanesi"
+        ###############################
+
+    #@BaseService.login_required
     def get(self):
         count_waiting_orders = f"SELECT COUNT(*) as count FROM Drug_Order " +\
                                f"WHERE pharmacy_id = {self.uid} AND order_status = 'pending approval'"
 
-        order_count = self.data_service.dql(query=count_waiting_orders, columns=["count"])["count"][0]
+        order_count = self.database_service.dql(query=count_waiting_orders, columns=["count"])["count"][0]
 
         return render_template("pharmacy/pharmacy_main.html", name=self.name, order_count=order_count)
